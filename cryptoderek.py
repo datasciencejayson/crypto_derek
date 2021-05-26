18ba0ad7865540d41cacff91fa4d6fc42a19373f
Jayson Backes <jaysonbackes@gmail.com>
2021-05-26 07:47:19 -0700

    Update cryptoderek.py

--- cryptoderek.py
+++ cryptoderek.py
@@ -1,7 +1,7 @@
+
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
-
 This is a temporary script file.
 """
 
@@ -14,6 +14,8 @@ from email.mime.multipart import MIMEMultipart
 
 email = "jaysonbackes@gmail.com"
 pas = "cGTpsL!dS3y2F"
+
+sms_gateway = '6155568530@txt.att.net'
 
 sms_gateway = '6158098494@vtext.com'
 # The server we use to send emails in our case it will be gmail but every email provider has a different smtp 
@@ -29,12 +31,12 @@ server.login(email,pas)
 
 # Now we use the MIME module to structure our message.
 msg = MIMEMultipart()
-msg['From'] = email
+msg['From'] = 'crypto cat'
 msg['To'] = sms_gateway
 # Make sure you add a new line in the subject
-msg['Subject'] = "You can insert anything\n"
+msg['Subject'] = "GME to the moon!"
 # Make sure you also add new lines to your body
-body = "You can insert message here\n"
+body = "2 tha moon222222222"
 # and then attach that body furthermore you can also send html content.
 msg.attach(MIMEText(body, 'plain'))
 
@@ -50,4 +52,3 @@ import cryptocompare
 cryptocompare.cryptocompare._set_api_key_parameter('2722bd1e76ffa497594e310b4605e5aa7e1a1d7a79fef5d08ac9d5b137c8867e')
 
 cryptocompare.get_price('BTC')
-
